294b4b9e640ba7421b2df6b13618527f45e4ac03
Yoshi Jaeger <github@jaeger.berlin>
2024-06-03 18:58:41 +0200

    feat(wrapper): created wrapper function that internally calls either format or parse, depending on the value's type

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,41 @@ pub mod format;
 pub use parse::parse;
 pub use format::format;
 
+pub fn ms<T: Into<MsInput>>(input: T) -> Result<MsOutput, &'static str> {
+    match input.into() {
+        MsInput::Str(s) => parse(&s).map(MsOutput::Milliseconds),
+        MsInput::Int(i) => Ok(MsOutput::Str(format(i))),
+    }
+}
+
+pub enum MsInput {
+    Str(String),
+    Int(u64),
+}
+
+impl From<&str> for MsInput {
+    fn from(s: &str) -> Self {
+        MsInput::Str(s.to_string())
+    }
+}
+
+impl From<String> for MsInput {
+    fn from(s: String) -> Self {
+        MsInput::Str(s)
+    }
+}
+
+impl From<u64> for MsInput {
+    fn from(i: u64) -> Self {
+        MsInput::Int(i)
+    }
+}
+
+pub enum MsOutput {
+    Str(String),
+    Milliseconds(u64),
+}
+
 // Some basic tests for this library.
 // ToDo: Add more tests here. I needed to craft those quickly.
 #[cfg(test)]
@@ -23,5 +58,17 @@ mod tests {
         assert_eq!(format(60_000), "1 minute");
         assert_eq!(format(999), "999 ms");
     }
-}
 
+    #[test]
+    fn test_ms_wrapper() {
+        match ms("2 days").unwrap() {
+            MsOutput::Milliseconds(ms) => assert_eq!(ms, 172_800_000),
+            _ => panic!("Expected milliseconds"),
+        }
+        match ms(60_000).unwrap() {
+            MsOutput::Str(s) => assert_eq!(s, "1 minute"),
+            _ => panic!("Expected string"),
+        }
+        assert!(ms("unknown").is_err());
+    }    
+}
